7e99de9ed92c62946fe3ce694808a804bd0b1cd1
Andrei Huryn <gurin.nord@gmail.com>
2023-12-10 14:27:10 +0300

    feat: add video to enjoy section of home page

--- coffee-house/menu/script.js
+++ coffee-house/menu/script.js
@@ -6,6 +6,9 @@ const teaPrice = document.querySelectorAll('.menu__card-price');
 const buttons = document.querySelectorAll('.menu__button');
 const buttonImg = document.querySelectorAll('.button__img');
 const menuCards = document.querySelectorAll('.menu__cards');
+const buttonRefresh = document.querySelector('.menu__button-refresh');
+const menuCardWrapper = document.querySelectorAll('.menu__card-wrapper');
+const cardWapperHidden = document.querySelectorAll('.card-wrapper__hidden');
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', () => {
@@ -20,9 +23,27 @@ for (let i = 0; i < buttons.length; i++) {
     buttonImg[i].classList.add('button__img_active');
 
     for (let cards of menuCards) {
-      cards.classList.add('fadein');
       cards.classList.remove('menu__cards_active');
+      cards.classList.add('fadein');
     }
     menuCards[i].classList.add('menu__cards_active');
+
+    if (i === 1) {
+      buttonRefresh.classList.add('button-refresh__invisible');
+    } else {
+      buttonRefresh.classList.remove('button-refresh__invisible');
+    }
+    
+    cardWapperHidden.forEach(el => {
+      el.classList.add('card-wrapper__hidden');
+    })
   })
 }
+
+buttonRefresh.addEventListener('click', () => {
+  for (let item of cardWapperHidden) {
+    item.classList.remove('card-wrapper__hidden');
+    item.classList.add('fadein');
+  }
+  buttonRefresh.classList.add('button-refresh__invisible');
+})
